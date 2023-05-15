6576bb415c08852a0cace684beb606ca5859ae4f
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2023-05-14 23:14:12 -0600

    Agregando funcionalidad Number

--- TypeSecure/src/main/java/com/peter/typesecure/ejecucion/instrucciones/Function_Number.java
+++ TypeSecure/src/main/java/com/peter/typesecure/ejecucion/instrucciones/Function_Number.java
@@ -6,23 +6,83 @@ package com.peter.typesecure.ejecucion.instrucciones;
 
 import com.peter.typesecure.ejecucion.Genericos.Instruction;
 import com.peter.typesecure.ejecucion.Genericos.SymbolTable;
+import com.peter.typesecure.ejecucion.Genericos.Variable;
+import com.peter.typesecure.ejecucion.Genericos.VariableType;
+import com.peter.typesecure.error.Error_analizadores;
 
 /**
  *
  * @author GORDILLOG
  */
-public class Function_Number extends Instruction{
+public class Function_Number extends Instruction {
 
     private Instruction operator;
-    
-    public Function_Number(Object linea, Object columna,Object operator) {
+
+    public Function_Number(Object linea, Object columna, Object operator) {
         super(linea, columna);
         this.operator = (Instruction) operator;
     }
 
     @Override
     public Object ejecutar(SymbolTable table) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        Variable var = (Variable) operator.ejecutar(table);
+
+        if (var != null) {
+
+            if (var.getValue() != null) {
+
+                Variable new_Var = new Variable();
+                new_Var.setType(VariableType.NUMBER);
+
+                switch (var.getType()) {
+                    case NUMBER:
+                        new_Var.setValue((Double) var.getValue());
+                        return new_Var;
+
+                    case BIGINT:
+                        String value_string = (String) var.getValue();
+                        Double value_number = Double.parseDouble(value_string.substring(0, value_string.length() - 1));
+                        new_Var.setValue(value_number);
+                        return new_Var;
+                    case BOOLEAN:
+                        if ((Boolean) var.getValue() == true) {
+                            new_Var.setValue(1.0);
+                            return new_Var;
+                        } else {
+                            new_Var.setValue(0.0);
+                            return new_Var;
+                        }
+                    case STRING:
+                        String value_str = (String) var.getValue();
+
+                        if (value_str.isBlank()==false) {
+                            try {
+                                Double value = Double.parseDouble(value_str);
+                                new_Var.setValue(value);
+                                return new_Var;
+                            } catch (Exception e) {
+                                table.agrearErrores(new Error_analizadores("Semantico", var.getValue().toString(), this.getLinea(), this.getColumna(), "El argumento de la funcion puede convertirse a number, no contiene un valor numerico"));
+                                return null;
+                            }
+                        } else {
+                            double val_double = 0;
+                            new_Var.setValue(val_double);
+                            return new_Var;
+                        }
+                }
+
+            } else {
+                table.agrearErrores(new Error_analizadores("Semantico", "", this.getLinea(), this.getColumna(), "El argumento de la funcion Number no tiene un valor definido"));
+                return null;
+            }
+
+        } else {
+            table.agrearErrores(new Error_analizadores("Semantico", "", this.getLinea(), this.getColumna(), "El argumento de la funcion Number no esta definido"));
+            return null;
+        }
+
+        return null;
     }
 
     public Instruction getOperator() {
@@ -37,7 +97,5 @@ public class Function_Number extends Instruction{
     public String toString() {
         return "Function_Number{" + "operator=" + operator + '}';
     }
-    
-    
-    
+
 }
