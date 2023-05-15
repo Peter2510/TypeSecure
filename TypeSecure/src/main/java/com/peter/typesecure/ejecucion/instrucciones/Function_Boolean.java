ae8217b099cb9e4653a55ea5d10b716cc0c25352
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2023-05-15 02:00:24 -0600

    Cambios parser funcion boolean

--- TypeSecure/src/main/java/com/peter/typesecure/ejecucion/instrucciones/Function_Boolean.java
+++ TypeSecure/src/main/java/com/peter/typesecure/ejecucion/instrucciones/Function_Boolean.java
@@ -6,6 +6,8 @@ package com.peter.typesecure.ejecucion.instrucciones;
 
 import com.peter.typesecure.ejecucion.Genericos.Instruction;
 import com.peter.typesecure.ejecucion.Genericos.SymbolTable;
+import com.peter.typesecure.ejecucion.Genericos.Variable;
+import com.peter.typesecure.ejecucion.Genericos.VariableType;
 
 /**
  *
@@ -22,7 +24,63 @@ public class Function_Boolean extends Instruction {
 
     @Override
     public Object ejecutar(SymbolTable table) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Variable var_tmp = (Variable) operator.ejecutar(table);
+        System.out.println(var_tmp);
+        Variable var = new Variable();
+        var.setType(VariableType.BOOLEAN);
+        Boolean val_f = false;
+        Boolean val_v = true;
+        if(var_tmp!=null){
+            
+            if(var_tmp.getValue()!=null && !"undefined".equals(var_tmp.getValue().toString())){
+            
+                switch (var_tmp.getType()) {
+                    case BOOLEAN:
+                        var.setValue((Boolean)var_tmp.getValue());
+                        return var;
+                    case NUMBER:   
+                        if((Double)var_tmp.getValue()==0){
+                            var.setValue(val_f);
+                            return var;                            
+                        }else{
+                            var.setValue(val_v);    
+                            return var;
+                        }
+                    case BIGINT:
+                        String value_big_string = (String)var_tmp.getValue();
+                        int value_bit_int = Integer.parseInt(value_big_string.substring(0,value_big_string.length()-1));
+                        if(value_bit_int!=0){
+                            var.setValue(val_v);
+                            return var;                              
+                        }else{
+                            var.setValue(val_f);
+                            return var;                              
+                        }                       
+                    case STRING:
+                        String value_string = (String)var_tmp.getValue();
+                        if(value_string.isBlank()){
+                            var.setValue(val_f);
+                            return var;                                                          
+                        }else{
+                            var.setValue(val_v);
+                            return var;                                                          
+                        }
+
+                }
+                
+            }else{
+
+                var.setValue(val_f);
+                return var;
+            }
+            
+        }else{
+
+            var.setValue(val_f);
+            return var;
+        }
+        
+        return null;
     }
 
     public Instruction getOperator() {
